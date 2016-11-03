128c0bbecfdbb93e0444a9d07a38b72b00cd47d7
Brynley Pearlstonevff <b.pearlstone.1@research.gla.ac.uk>
2016-11-03 09:57:21 +0000

    Maths for coherent_RBB (reduced BB) fixed, must fix barcode diagram

--- codes/coherent_blocks/logaddexpvect.m
+++ codes/coherent_blocks/logaddexpvect.m
@@ -4,39 +4,6 @@ function [ y ] = logaddexpvect( x )
 %   Input 2 log values to be summed. Output double precision
 %   output which is the sum of the inputs.
 
-% e_vect = exp(x);
-% sum_vect = sum(e_vect);
-% y = log(sum_vect);
-% This is the by-the numbers way to do it. This often results in inf and
-% -inf outputs
-% 
-% running_answer = x(1);
-% if length(x)>1;
-%     for itteration = 2:length(x)
-%         if x(itteration)<running_answer
-%             new_answer = log(exp(x(itteration) - running_answer) + 1) + running_answer;
-%         else
-%             new_answer = log(exp(running_answer - x(itteration) ) + 1) + x(itteration);
-%         end
-%         running_answer = new_answer;
-%     end
-% end
-% y = running_answer;
-% This is my first try at a fix following the standard method. This still
-% sometimes results in +/-inf values for high SNR
-
-% running_answer = x(1);
-% if length(x)>1
-%     for itteration = 2:length(x)
-%         new_answer = max(running_answer, x(itteration)) + log(exp(running_answer - max(running_answer, x(itteration)) ) + exp(x(itteration) - max(running_answer, x(itteration)) ));
-%         running_answer = new_answer;
-%     end
-% end
-% y = running_answer;
-% This third attempt is an approximation which should prevent overflow and
-% underflow, but still result in some infinities.
-
-
 running_answer = -inf;
 for itteration = 1:length(x)
    running_answer = logplus(running_answer, x(itteration));
