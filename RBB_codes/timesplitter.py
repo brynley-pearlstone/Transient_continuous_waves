7b236724a08176c1ade3280a028006247910090b
Brynley Pearlstone <spxbp1@raven14.bullx>
2017-12-11 10:37:14 +0000

    Changed timesplitter to chunk up data evenly

--- RBB_codes/timesplitter.py
+++ RBB_codes/timesplitter.py
@@ -17,7 +17,6 @@ parser.add_argument("-o", "--outfile", dest = "outfile",
 parser.add_argument("-d", "--detector", dest = "detector",
                   help = "Detector Name eg H1, L1, V1", metavar = "STRING")
 
-
 args = parser.parse_args()
 
 n_chunks = int(args.n_chunks)
@@ -33,23 +32,15 @@ if outfile[-1] != '/':
 	outfile = outfile + '/'
 if not os.path.exists(outfile):
         os.makedirs(outfile)
-#	os.makedirs(outfile + 'H1/')
-#	os.makedirs(outfile + 'L1/')
 elif not os.path.exists(outfile + detector_name + '/'):
 	os.makedirs(outfile + detector_name + '/')
 
 lines = []
-f = open(filename, 'r')
-for line in f:
-	lines.append(line[:-1])
-
-#table_headers = lines[0:14]
-#lines[0:14]= []
-splitline = [line.split('	') for line in lines]
-
-
-f.close
-
+#f = open(filename, 'r')
+with open(filename, 'r') as f:
+	for line in f:
+		lines.append(line[:-1])
+	splitline = [line.split('	') for line in lines]
 
 
 time_length = float(splitline[-1][0]) - float(splitline[0][0])
@@ -58,29 +49,18 @@ for i in range(n_chunks):
 	chunk_time.append(float(splitline[0][0]) + float(i * (time_length / n_chunks)))
 chunk_time.append(float(splitline[-1][0]))
 print(chunk_time)
+
+for i in range(len(chunk_time)):
+	new_time = chunk_time[i] - 0.5
+	chunk_time[i] = new_time	
+
+chunk_time[-1] += 1
+ 
 for j in range(n_chunks):
 	for k in range(len(chunk_time)):
 		if k>j:
-			output_file = open(outfile + detector_name + '/' + detector_name + '-chunk_' + str(j) + '_to_' + str(k) + '.txt', 'w+')
-			output_file.seek(0)
-			#for item in table_headers:
-				#output_file.write(str(item) + '\n')
-			for l in splitline:
-				if float(l[0]) > chunk_time[j] and float(l[0]) < chunk_time[k]:
-					output_file.write(str(l[0]) + '     ' + str(l[1]) + '     ' + str(l[2]) +  ' \n')
-			output_file.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+			with open(outfile + detector_name + '/' + detector_name + '-chunk_' + str(j) + '_to_' + str(k) + '.txt', 'w+') as output_file:
+				output_file.seek(0)
+				for l in splitline:
+					if float(l[0]) >= chunk_time[j] and float(l[0]) <= chunk_time[k]:
+						output_file.write(str(l[0]) + '\t' + str(l[1]) + '\t' + str(l[2]) +  ' \n')
